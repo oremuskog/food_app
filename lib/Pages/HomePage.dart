6796e9468a8c0cd823a6107be054bf0d2b88be1e
Ørémus <salesodjo@gmail.com>
2023-10-10 03:07:13 -0700

    yeah this is first commit

---
+++ lib/Pages/HomePage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../Widgets/AppBarWidget.dart';
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: ListView(
+        children: [
+          // Custom App Bar Widget
+          AppBarWidget(),
+        ],
+      ),
+    );
+  }
+}
